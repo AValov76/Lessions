7c31e638b2c5e11ec9e6c32078a16716f1906df8
AValov76 <43465649+AValov76@users.noreply.github.com>
2019-09-28 23:15:27 +0300

    home work

--- src/Groups.java
+++ src/Groups.java
@@ -1,39 +1,45 @@
 //: strings/Groups.java
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
 
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.regex.*;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Groups {
-    static void print(){System.out.println();}
-    static void print(Object o){
+    private static void print() {
+        System.out.println();
+    }
+
+    private static void print(Object o) {
         System.out.print(o);
     }
-    static public final String POEM =
+
+    private static final String POEM =
             "Twas brillig, and the slithy toves\n" +
                     "Did gyre and gimble in the wabe.\n" +
                     "All mimsy were the borogoves,\n" +
                     "And the mome raths outgrabe.\n\n" +
                     "Beware the Jabberwock, my son,\n" +
                     "The jaws that bite, the claws that catch.\n" +
-                    "Beware the the the the Jubjub bird, and shun\n" +
+                    "Beware the Jubjub bird, and shun\n" +
                     "The frumious Bandersnatch.";
+
     public static void main(String[] args) {
         Matcher m =
                 Pattern.compile("(\\w+)")
                         .matcher(POEM);
-        int i=0;
-        HashSet a = new HashSet();
-        while(m.find()) {
-                a.add(m.group());
-                i++;
+        int i = 0;
+        Set<String> a = new TreeSet<>();
+        while (m.find()) {
+            a.add(m.group());
+            i++;
 
         }
         print(a);
         print();
         print(i);
+        print();
+        print(a.size());
     }
 }
